23dd745c877e63dc14678acdd3158c4fbb7f9cae
Manuel Ulate <fr3zs09@gmail.com>
2024-04-23 22:42:45 -0600

    new: matterport map-frame component mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { MapFrame } from './_components/map-frame';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      
+      <MapFrame />
     </main>
   );
 }
